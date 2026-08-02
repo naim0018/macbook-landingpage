a92cec868eb01bb82383249325c8b196962417ec
mdkazinaim <kazinaim121@gmail.com>
2026-08-02 14:17:01 +0600

    02Aug,26 02:17 PM - updated: M4Mask,Product PublicRoutes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -8,29 +8,4 @@ export const publicRoutes = [
     path: "/",
     element: <Home />,
   },
-  {
-    label: "Mac",
-    path: "/mac",
-    element: <Home />,
-  },
-  {
-    label: "iPhone",
-    path: "/iPhone",
-    element: <Home />,
-  },
-  {
-    label: "Watch",
-    path: "/watch",
-    element: <Home />,
-  },
-  {
-    label: "Vision",
-    path: "/vision",
-    element: <Home />,
-  },
-{
-    label: "AirPods",
-    path: "/airpods",
-    element: <Home />,
-  }
 ];
